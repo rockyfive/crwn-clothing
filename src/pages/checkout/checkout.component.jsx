9e732582871f79bc6043b5ec58f0dd72e7aca259
Pedro Garcia Esteban <pedro.garciaesteban@amadeus.com>
2020-02-04 17:51:16 +0100

    Moved directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,7 +7,6 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
-import CartItem from '../../components/cart-item/cart-item.component';
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
